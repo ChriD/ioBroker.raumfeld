1ae1a552d0751a3d562514ee997182c25d7a1258
Christian <CDuernberger8463@cosmoconsult.com>
2023-01-27 21:37:36 +0100

    initial

--- main.js
+++ main.js
@@ -7,7 +7,8 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
-const { Raumkernel } = require('D:\\Projects\\Raumfeld\\node-raumkernel');
+//const { Raumkernel } = require('D:\\Projects\\Raumfeld\\node-raumkernel');
+const { Raumkernel } = require('node-raumkernel');
 
 // Load your modules here, e.g.:
 // const fs = require("fs");
